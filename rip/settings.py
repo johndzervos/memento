b1ad30ba7a620c5e58f4bc62e99e12ad1696e369
johndzervos <ioannis@crunchrapps.com>
2021-11-24 23:05:12 +0100

    Static files

--- rip/settings.py
+++ rip/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
